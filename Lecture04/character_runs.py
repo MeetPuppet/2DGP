7b45c83ed235c432425894e0a7feb3a6c2bad34b
Minkyeong <gangriming@naver.com>
2018-09-13 18:40:52 +0900

    강의종료
    스프라이트, 애니메이션 적용방법

--- Lecture04/character_runs.py
+++ Lecture04/character_runs.py
@@ -5,7 +5,17 @@ character = load_image('run_animation.png')
 
 x = 0
 
-# 여기를 채우세요.
+frame=0
+
+while(x<800):
+    clear_canvas()
+    grass.draw(400,30)
+    character.clip_draw(frame*100,0,100,100,x,90)
+    update_canvas()
+    frame=(frame+3)%8
+    x+=15
+    delay(0.05)
+    get_events()
 
 
 close_canvas()
