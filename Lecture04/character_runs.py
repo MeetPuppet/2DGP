14f47a766fb3e095faca1100654c213ed6b57dc5
Minkyeong <gangriming@naver.com>
2018-09-13 19:00:16 +0900

    drill 04 완료
    2013182055 최치송
    https://youtu.be/veh-bTTku2A

--- Lecture04/character_runs.py
+++ Lecture04/character_runs.py
@@ -1,21 +1,36 @@
 from pico2d import *
 open_canvas()
 grass = load_image('grass.png')
-character = load_image('run_animation.png')
+character = load_image('animation_sheet.png')
 
-x = 0
+x=0
+mode = 0
 
-frame=0
-
-while(x<800):
-    clear_canvas()
-    grass.draw(400,30)
-    character.clip_draw(frame*100,0,100,100,x,90)
-    update_canvas()
-    frame=(frame+3)%8
-    x+=15
-    delay(0.05)
-    get_events()
+frameX=0
+frameY=1
+while(True):
+    if(mode==0):
+        clear_canvas()
+        grass.draw(400,30)
+        character.clip_draw(frameX*100,frameY*100,100,100,x,90)
+        update_canvas()
+        frameX=(frameX+1)%8
+        x+=10
+        delay(0.05)
+        if(x>800):
+            mode=1
+            frameY=0
+    elif(mode==1):
+        clear_canvas()
+        grass.draw(400,30)
+        character.clip_draw(frameX*100,frameY*100,100,100,x,90)
+        update_canvas()
+        frameX=(frameX+1)%8
+        x-=10
+        delay(0.05)
+        if(x<0):
+            mode=0
+            frameY=1
 
 
 close_canvas()
