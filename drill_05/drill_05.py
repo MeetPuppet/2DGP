cc108c3e66c93ae90a5b1a4b6cdfe0565a319565
Chisong(PC-17) <zw159@naver.com>
2018-09-18 18:37:02 +0900

    drill_05_01
    1번 커밋
    기본셋팅

---
+++ drill_05/drill_05.py
@@ -0,0 +1,14 @@
+from pico2d import *
+
+open_canvas()
+
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+def move_to_there(characterX,characterY,toX,toY):
+
+    pass
+
+x, y = 800 // 2, 90
+while True :
+    move_to_there()
