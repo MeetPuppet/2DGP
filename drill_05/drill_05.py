540ffcaf623a17584a2e38532c568a5c66e45701
Chisong(PC-17) <zw159@naver.com>
2018-09-18 18:57:44 +0900

    Revert "drill_05_02"
    This reverts commit 175c6cbad1c833508c9d77c907a07f3720b070b1.

--- drill_05/drill_05.py
+++ drill_05/drill_05.py
@@ -3,52 +3,12 @@ from pico2d import *
 open_canvas()
 
 grass = load_image('grass.png')
-character = load_image('animation_sheet.png')
+character = load_image('character.png')
 
-def move_to_there_01():
-        pass
+def move_to_there(characterX,characterY,toX,toY):
 
-
-def move_to_there_02():
     pass
 
-
-def move_to_there_03():
-    pass
-
-
-def move_to_there_04():
-    pass
-
-
-def move_to_there_05():
-    pass
-
-
-def move_to_there_06():
-    pass
-
-
-def move_to_there_07():
-    pass
-
-
-def move_to_there_08():
-    pass
-
-
-def move_to_there_09():
-    pass
-
-
-def move_to_there_10():
-    pass
-
-
+x, y = 800 // 2, 90
 while True :
-    move_to_there_01()
-
-
-
-
-close_canvas()
+    move_to_there()
