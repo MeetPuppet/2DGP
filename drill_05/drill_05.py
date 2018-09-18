b7bf714391cef5dd45142b6277de1e8aceb729d9
Chisong(PC-17) <zw159@naver.com>
2018-09-18 19:29:53 +0900

    drill_05_05
    5번째 커밋
    move_to_there 작성중
    move_to_there_XX공통점 발견
    적용방식 변경하여 함수move_arround추가

--- drill_05/drill_05.py
+++ drill_05/drill_05.py
@@ -5,88 +5,32 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 
-
-def move_to_there_01():
-    x, y=712, 349
+def move_to_there(startX,startY,endX,endY):
+    x, y=startX, startY
     frameX=0
     frameY=0
-    while x>203 or y<535:
+    while x!=endX or y!=endY:
         clear_canvas()
         grass.draw(400, 30)
         character.clip_draw(frameX * 100, frameY * 100, 100, 100, x, y)
         update_canvas()
-        frameX = (frameX + 1) % 8
-        if x > 203:
-            x-=1
+        frameX = (frameX + 3) % 8
+        if x > endX:
+            x-=0.5
             frameY=0
-        elif x < 203:
-            x+=1
+        elif x<endX:
+            x+=0.5
             frameY=1
 
-        if y < 535:
-            y+=1
-        elif y > 535:
-            y-=1
-
-def move_to_there_02():
-    x, y=203, 535
-    frameX=0
-    frameY=0
-    while x>132 or y>243:
-        clear_canvas()
-        grass.draw(400, 30)
-        character.clip_draw(frameX * 100, frameY * 100, 100, 100, x, y)
-        update_canvas()
-        frameX = (frameX + 1) % 8
-        if x > 132:
-            x-=1
-            frameY=0
-        elif x<132:
-            x+=1
-            frameY=1
-
-        if y < 243:
-            y+=1
-        elif y>243:
-            y-=1
-
-
-def move_to_there_03():
-    pass
-
-
-def move_to_there_04():
-    pass
-
-
-def move_to_there_05():
-    pass
-
-
-def move_to_there_06():
-    pass
-
-
-def move_to_there_07():
-    pass
-
-
-def move_to_there_08():
-    pass
-
-
-def move_to_there_09():
-    pass
-
-
-def move_to_there_10():
-    pass
+        if y < endY:
+            y+=0.5
+        elif y>endY:
+            y-=0.5
 
 def move_arround():
-    move_to_there_01()
-    move_to_there_02()
-
-
+    move_to_there(712,349,203,530)
+    move_to_there(203,535,132,243)
+    move_to_there(132,243,535,470)
 while True :
     move_arround()
     pass
