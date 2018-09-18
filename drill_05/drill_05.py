4864dd5f1e78727bc384f9c27185246a3371bfc6
Chisong(PC-17) <zw159@naver.com>
2018-09-18 19:07:18 +0900

    drill_05_03
    3번째 커밋
    move_to_there_01완성

--- drill_05/drill_05.py
+++ drill_05/drill_05.py
@@ -7,7 +7,28 @@ character = load_image('animation_sheet.png')
 
 
 def move_to_there_01():
-    pass
+    x, y=712, 349
+    frameX=0
+    frameY=0
+    while x>203 or y<535:
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frameX * 100, frameY * 100, 100, 100, x, y)
+        update_canvas()
+        frameX = (frameX + 1) % 8
+        if x > 203+1:
+            x-=10
+            frameX=0
+        else:
+            x+=1
+            frameX=1
+
+        if y < 535+1:
+            y+=10
+        else:
+            y-=1
+
+
 
 
 def move_to_there_02():
@@ -38,14 +59,13 @@ def move_to_there_08():
     pass
 
 
-x, y = 800 // 2, 90
-
-
 def move_to_there_09():
     pass
 
 
 def move_to_there_10():
     pass
+
 while True :
     move_to_there_01()
+    pass
