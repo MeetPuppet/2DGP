175c6cbad1c833508c9d77c907a07f3720b070b1
Chisong(PC-17) <zw159@naver.com>
2018-09-18 18:57:30 +0900

    drill_05_02
    기본셋팅 조정

--- drill_05/drill_05.py
+++ drill_05/drill_05.py
@@ -3,12 +3,52 @@ from pico2d import *
 open_canvas()
 
 grass = load_image('grass.png')
-character = load_image('character.png')
+character = load_image('animation_sheet.png')
 
-def move_to_there(characterX,characterY,toX,toY):
+def move_to_there_01():
+        pass
 
+
+def move_to_there_02():
     pass
 
-x, y = 800 // 2, 90
+
+def move_to_there_03():
+    pass
+
+
+def move_to_there_04():
+    pass
+
+
+def move_to_there_05():
+    pass
+
+
+def move_to_there_06():
+    pass
+
+
+def move_to_there_07():
+    pass
+
+
+def move_to_there_08():
+    pass
+
+
+def move_to_there_09():
+    pass
+
+
+def move_to_there_10():
+    pass
+
+
 while True :
-    move_to_there()
+    move_to_there_01()
+
+
+
+
+close_canvas()
