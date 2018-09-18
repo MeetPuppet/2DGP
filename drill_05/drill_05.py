3307d1a6c80ac4f25a95e96f1ded59b773e5adee
Chisong <eogkdgkwk@gmail.com>
2018-09-18 20:00:13 +0900

    drill_05_06
    6번째 커밋
    4~10번째 좌표까지 적용
    촬영을 위해 속도 재조정
    함수 이름 직관화
    move_to_there를 move_from_to

--- drill_05/drill_05.py
+++ drill_05/drill_05.py
@@ -5,7 +5,7 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 
-def move_to_there(startX,startY,endX,endY):
+def move_from_to(startX,startY,endX,endY):
     x, y=startX, startY
     frameX=0
     frameY=0
@@ -14,23 +14,31 @@ def move_to_there(startX,startY,endX,endY):
         grass.draw(400, 30)
         character.clip_draw(frameX * 100, frameY * 100, 100, 100, x, y)
         update_canvas()
-        frameX = (frameX + 3) % 8
+        frameX = (frameX + 1) % 8
         if x > endX:
-            x-=0.5
+            x-=1
             frameY=0
         elif x<endX:
-            x+=0.5
+            x+=1
             frameY=1
 
         if y < endY:
-            y+=0.5
+            y+=1
         elif y>endY:
-            y-=0.5
+            y-=1
 
 def move_arround():
-    move_to_there(712,349,203,530)
-    move_to_there(203,535,132,243)
-    move_to_there(132,243,535,470)
+    move_from_to(712,349,203,530)
+    move_from_to(203,535,132,243)
+    move_from_to(132,243,535,470)
+    move_from_to(535,470,477,203)
+    move_from_to(477,203,715,136)
+
+    move_from_to(715,136,316,225)
+    move_from_to(316,225,510,92)
+    move_from_to(510,92,692,518)
+    move_from_to(692,518,682,336)
+    move_from_to(682,336,712,349)
 while True :
     move_arround()
     pass
