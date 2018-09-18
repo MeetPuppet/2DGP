b5206cf862bcb946ae6ba1e84fd2775212161192
Chisong(PC-17) <zw159@naver.com>
2018-09-18 17:49:52 +0900

    drill_05_1 코드기본구조
    코드의 기본구조 완성
    
    바로 코딩시작하지 말 것

---
+++ drill_05/drill_05.py
@@ -0,0 +1,26 @@
+from pico2d import *
+
+open_canvas()
+
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+def make_rectangle():
+    pass
+
+def make_circle():
+    pass
+
+while True:
+    make_rectangle()
+    make_circle()
+
+
+
+close_canvas()
+
+"""
+함수의 이름에서 앞 단어는 동사여야한다.
+ex. rectangle -> make_rectangle
+
+"""
