0b7400bbd93025e6dec48fcf0cb16a00e7dc7173
user(PC11) <>
2018-10-02 18:49:24 +0900

    drill 07 기본셋팅

---
+++ drill_07/drill_07.py
@@ -0,0 +1,13 @@
+from pico2d import *
+
+open_canvas()
+
+back = load_image('KPU_GROUND.png')
+character = load_image('animation_sheet.png')
+
+def move_from_to(p1,p2)
+    pass
+
+
+while True:
+    pass
