722ed9104c84e6a7c65fa049d201ca2c9f992521
user(PC11) <>
2018-10-02 19:01:03 +0900

    drill 07 함수작성

--- drill_07/drill_07.py
+++ drill_07/drill_07.py
@@ -1,13 +1,28 @@
 from pico2d import *
+import random
 
 open_canvas()
 
 back = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
 
-def move_from_to(p1,p2)
-    pass
+def move_from_to(p1,p2):
+    start = p1
+    end = p2
+    global frame
+    t = 0
 
+    while t >= 1:
+        clear_canvas()
+        back.draw(400,300)
+        character.clip_draw(frame[0]*100,frame[1]*100,100,100,start[0]*(1-t)+end[0]*t)
+        update_canvas()
+        frame[0] = (frame[0]+1)%8
+        if end[0]<start[0]:
+            frame[1]=1
+        else:
+            frame[1]=0
 
 while True:
+
     pass
