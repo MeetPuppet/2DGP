ea91b2b1dc29ab95230c0182dfb92a3587dce251
user(PC11) <>
2018-10-04 18:18:13 +0900

    drill 08 01 틀작성

--- drill_08/drill_08.py
+++
@@ -1,5 +0,0 @@
-from pico2d import *
-import random
-
-def draw_p4_line(prev,start,end,next):
-    pass
---
+++ drill_08/drill_08_01.py
@@ -0,0 +1,13 @@
+from pico2d import *
+i
+
+def draw_p4_line(prev,start,end,next):
+    pass
+
+points=[(-300,200),(400,350),(300,-300),(-200,-200)]
+
+arrow=0
+size=len(points)
+
+while True:
+    pass
