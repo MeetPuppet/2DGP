05265bec80894ea1b46132b93f28785875a982b3
user(PC11) <>
2018-10-04 18:21:53 +0900

    drill 08 01 캔버스 이동

--- drill_08/drill_08_01.py
+++ drill_08/drill_08_01.py
@@ -1,7 +1,48 @@
-from pico2d import *
-i
+import turtle
+import random
+
+
+def stop():
+    turtle.bye()
+
+
+def prepare_turtle_canvas():
+    turtle.setup(1024, 768)
+    turtle.bgcolor(0.2, 0.2, 0.2)
+    turtle.penup()
+    turtle.hideturtle()
+    turtle.shape('arrow')
+    turtle.shapesize(2)
+    turtle.pensize(5)
+    turtle.color(1, 0, 0)
+    turtle.speed(100)
+    turtle.goto(-500, 0)
+    turtle.pendown()
+    turtle.goto(480, 0)
+    turtle.stamp()
+    turtle.penup()
+    turtle.goto(0, -360)
+    turtle.pendown()
+    turtle.goto(0, 360)
+    turtle.setheading(90)
+    turtle.stamp()
+    turtle.penup()
+    turtle.home()
+
+    turtle.shape('circle')
+    turtle.pensize(1)
+    turtle.color(0, 0, 0)
+    turtle.speed(50)
+
+    turtle.onkey(stop, 'Escape')
+    turtle.listen()
+
 
 def draw_p4_line(prev,start,end,next):
+    t=0
+    while t<=1+0.01:
+        x=
+        t+=0.01
     pass
 
 points=[(-300,200),(400,350),(300,-300),(-200,-200)]
