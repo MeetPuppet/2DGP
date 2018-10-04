f7a93a0d51ca176073c0ed4f76f392a2cd8a4188
user(PC11) <>
2018-10-04 18:41:39 +0900

    drill 08 01  적용

--- drill_08/drill_08_01.py
+++ drill_08/drill_08_01.py
@@ -49,19 +49,29 @@ def draw_point(p):
     turtle.dot(5, random.random(), random.random(), random.random())
 
 def draw_p4_line(prev,start,end,next):
-    t=0
     draw_big_point(start)
-    while t<=1+0.01:
-        x=((-t**3+2*t**2-t)*prev[0]+(3*t**3-5*t**2+2)*start[0]+(-3*t**3+4*t**2+t)*end[0]+(t**3-t**2)*next)*0.5
-        y=((-t**3+2*t**2-t)*prev[0]+(3*t**3-5*t**2+2)*start[0]+(-3*t**3+4*t**2+t)*end[0]+(t**3-t**2)*next)*0.5
+    t=0
+    for i in range(0,100,5):
+        x=((-t**3+2*t**2-t)*prev[0]+(3*t**3-5*t**2+2)*start[0]+(-3*t**3+4*t**2+t)*end[0]+(t**3-t**2)*next[0])*0.5
+        y=((-t**3+2*t**2-t)*prev[1]+(3*t**3-5*t**2+2)*start[1]+(-3*t**3+4*t**2+t)*end[1]+(t**3-t**2)*next[1])*0.5
+        t+=0.05
         draw_point((x,y))
-        t+=0.01
     draw_big_point(end)
+
+prepare_turtle_canvas()
 
 points=[(-300,200),(400,350),(300,-300),(-200,-200)]
 
-arrow=1
+arrow=0
 size=len(points)
 
 while True:
-    
+    p1=points[arrow%size]
+    arrow+=1
+    p2=points[arrow%size]
+    arrow+=1
+    p3=points[arrow%size]
+    arrow+=1
+    p4=points[arrow%size]
+    draw_p4_line(p1,p2,p3,p4)
+    arrow-=2
